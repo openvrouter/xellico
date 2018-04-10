ddd345cb71c57243f8b6c2a79e0c705447a4e4f8
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-04-10 22:33:58 +0900

    2018.04.10-22:33

--- misc/delay_eval/main.c
+++ misc/delay_eval/main.c
@@ -1,57 +1,80 @@
 
 #include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
-#include <sys/queue.h>
-
-#include <rte_memory.h>
-#include <rte_memzone.h>
 #include <rte_launch.h>
 #include <rte_eal.h>
-#include <rte_per_lcore.h>
 #include <rte_lcore.h>
-#include <rte_debug.h>
 #include <rte_cycles.h>
+#define MEGA (1000 * 1000)
+#define GIGA (1000 * 1000 * 1000)
+
 
 static inline void
-dirty_looped_delay (uint32_t nb_loop)
+dirty_looped_delay_10ns (uint32_t nsec)
 {
-  volatile uint32_t vcount = nb_loop;
+  volatile uint32_t vcount = nsec*8 + nsec;
   while (vcount --);
+}
+
+static void
+test_dirty_looped_delay_10ns (size_t nsec, size_t trycnt)
+{
+  size_t sum = 0;
+  for (size_t i=0; i<trycnt; i++)
+    {
+      size_t before = rte_get_tsc_cycles ();
+      dirty_looped_delay_10ns (nsec);
+      sum += rte_get_tsc_cycles () - before;
+    }
+  size_t latency = sum/trycnt;
+  size_t hz = rte_get_tsc_hz ();
+  printf ("dirty_looped_delay_10ns(%zd): "
+          " lat=%zd [clk] try=%zd, delay=%zdns\n",
+          nsec, latency, trycnt, GIGA*latency/hz);
+}
+
+static void
+test_rte_delay_us_block (size_t usec, size_t trycnt)
+{
+  size_t sum = 0;
+  for (size_t i=0; i<trycnt; i++)
+    {
+      size_t before = rte_get_tsc_cycles ();
+      rte_delay_us_block (usec);
+      sum += rte_get_tsc_cycles () - before;
+    }
+  size_t latency = sum/trycnt;
+  size_t hz = rte_get_tsc_hz ();
+  printf ("rte_delay_us_block(%zd): "
+          " lat=%zd [clk] try=%zd, delay=%zdus\n",
+          usec, latency, trycnt, MEGA*latency/hz);
 }
 
 static int
 func (__attribute__((unused)) void *arg)
 {
-  const size_t loopcnt = 1000000;
-  size_t sum = 0;
-  for (size_t i=0; i<loopcnt; i++)
-    {
-      size_t before = rte_get_tsc_cycles ();
-      rte_delay_us_block (2);
-      /* dirty_looped_delay (10); */
-      sum += rte_get_tsc_cycles () - before;
-    }
-  size_t latency = sum/loopcnt;
-  printf ("avg latency is %zd clock lcore%u\n",
-      latency, rte_lcore_id ());
+  printf ("freq: %zd\n", rte_get_tsc_hz ());
+  size_t trycnt = 100 * MEGA;
+  /* test_rte_delay_us_block (1, trycnt); */
+  /* test_rte_delay_us_block (2, trycnt); */
+  /* test_rte_delay_us_block (3, trycnt); */
+  /* test_rte_delay_us_block (4, trycnt); */
+  test_dirty_looped_delay_10ns (1, trycnt);
+  test_dirty_looped_delay_10ns (2, trycnt);
+  test_dirty_looped_delay_10ns (3, trycnt);
+  test_dirty_looped_delay_10ns (4, trycnt);
 	return 0;
 }
 
 int
 main (int argc, char **argv)
 {
+  rte_log_set_global_level (RTE_LOG_EMERG);
 	int ret = rte_eal_init (argc, argv);
 	if (ret < 0)
 		rte_panic ("Cannot init EAL\n");
 
-  printf ("\n\n");
   rte_eal_remote_launch (func, NULL, 1);
-  rte_eal_remote_launch (func, NULL, 2);
-  rte_eal_remote_launch (func, NULL, 3);
-  rte_eal_remote_launch (func, NULL, 4);
-  rte_eal_remote_launch (func, NULL, 5);
 	rte_eal_mp_wait_lcore ();
 	return 0;
 }
+
