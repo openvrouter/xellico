f2a292cc634bb6745efe923f765f562e0c756c46
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-04-21 17:53:31 +0900

    2018.04.21-17:53

--- misc/pktgen/autotest.lua
+++ misc/pktgen/autotest.lua
@@ -2,8 +2,8 @@ package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
 
 function setting(arg_pkt_size)
 	pktgen.set_range('all', 'on')
+	-- print('set range mode on')
 
-	print('set range mode on')
 	sip_sta = '10.0.0.1'
 	sip_min = '10.0.0.1'
 	sip_max = '10.0.255.255'
@@ -12,8 +12,8 @@ function setting(arg_pkt_size)
 	pktgen.src_ip('all', 'min'  , sip_min)
 	pktgen.src_ip('all', 'max'  , sip_max)
 	pktgen.src_ip('all', 'inc'  , sip_inc)
-	printf('src.ip %s [%s to %s] inc:%s\n',
-		sip_sta, sip_min, sip_max, sip_inc)
+	-- printf('src.ip %s [%s to %s] inc:%s\n',
+		-- sip_sta, sip_min, sip_max, sip_inc)
 
 	dip_sta = '10.10.0.1'
 	dip_min = '10.10.0.1'
@@ -23,8 +23,8 @@ function setting(arg_pkt_size)
 	pktgen.dst_ip('all', 'min'  , dip_min)
 	pktgen.dst_ip('all', 'max'  , dip_max)
 	pktgen.dst_ip('all', 'inc'  , dip_inc)
-	printf('dst.ip %s [%s to %s] inc:%s\n',
-		dip_sta, dip_min, dip_max, dip_inc)
+	-- printf('dst.ip %s [%s to %s] inc:%s\n',
+	-- 	dip_sta, dip_min, dip_max, dip_inc)
 
 	pktsize_sta = arg_pkt_size
 	pktsize_min = 0
@@ -34,8 +34,8 @@ function setting(arg_pkt_size)
 	pktgen.pkt_size('all', 'min'  , pktsize_min);
 	pktgen.pkt_size('all', 'max'  , pktsize_max);
 	pktgen.pkt_size('all', 'inc'  , pktsize_inc);
-	printf('pkt_size %d [%d to %d] inc:%d\n',
-		pktsize_sta, pktsize_min, pktsize_max, pktsize_inc)
+	-- printf('pkt_size %d [%d to %d] inc:%d\n',
+	-- 	pktsize_sta, pktsize_min, pktsize_max, pktsize_inc)
 	return
 end
 
@@ -49,35 +49,38 @@ function get_avg(arry, size)
 end
 
 
-function traffic_test(delay_msec, cnt)
+function test_throughput(delay_msec, cnt, key)
 	pktgen.clr();
 	pktgen.start('all');
 	local rx = {}
-	local tx = {}
 	pktgen.delay(2000)
 	for i=1, cnt, 1 do
 		local s = pktgen.portStats("all", "rate");
-		local cur_rx = s[0]['mbits_rx'] + s[1]['mbits_rx']
-		local cur_tx = s[0]['mbits_tx'] + s[1]['mbits_tx']
+		local cur_rx = s[0][key] + s[1][key]
 
 		table.insert(rx, cur_rx)
-		table.insert(tx, cur_tx)
-		printf("rx:%dMbps, tx:%dMbps \n", cur_rx, cur_tx)
+		printf("%s:%dMbps, \n", key, cur_rx)
 		pktgen.delay(delay_msec)
 	end
 	pktgen.stop('all');
-	return rx, tx
+	return rx
 end
 
 
 function main()
-	cnt = 3
-	setting(64)
-	local rx64, tx64 = traffic_test(1000, cnt)
-	setting(128)
-	local rx128, tx128 = traffic_test(1000, cnt)
-	print(64, get_avg(rx64, cnt), get_avg(tx64, cnt))
-	print(128, get_avg(rx128, cnt), get_avg(tx128, cnt))
+	local cnt = 1
+	local dum = 0
+
+	setting(64  ); local res64   = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(128 ); local res128  = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(192 ); local res192  = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(256 ); local res256  = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(512 ); local res512  = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(1024); local res1024 = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+	setting(1514); local res1514 = get_avg(test_throughput(1000, cnt, 'mbits_rx'), cnt);
+
+	printf("%10d, %10d, %10d, %10d, %10d, %10d, %10d\n",
+		res64, res128, res192, res256, res512, res1024, res1514)
 end
 
 
